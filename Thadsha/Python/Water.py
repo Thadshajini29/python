843f2785e5c5dce4524211135031803dfbca9361
Thadshajini29 <sajiiselvi@gmail.com>
2025-08-05 17:03:44 +0530

    while loop,nextedloop

--- Thadsha/Python/Water.py
+++ Thadsha/Python/Water.py
@@ -1,14 +1,17 @@
 water = input("Enter the use waterliter: ")
 liter = float(water)
+bill=3;
+bill2=5;
+bill3=10;
 if liter>0:
     if liter>=0 and liter<=1000 :
         price = 500
     elif liter>=1001 and liter<=3000:
-        price = 500 + (liter-1000)*3
+        price = 500 + (liter-1000)*bill
     elif liter>=3001 and liter<=10000:
-        price = 500 + 2000*3 + (liter-3000)*5
+        price = 500 + 2000*bill + (liter-3000)*bill2
     else:
-        price = 500 + 2000*3 +7000*5 +(liter-10000)*10
+        price = 500 + 2000*bill +7000*bill2 +(liter-10000)*bill3
     print(f"Price = {price}")
 else:
     print(" Enter the correct waterliter ")
