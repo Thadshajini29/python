1ee4231d47882920add7b0dc763e980e1e7d6347
Thadshajini29 <sajiiselvi@gmail.com>
2025-08-06 16:49:50 +0530

    Add for loop examples and update while loop logic
    Added a new file forloop.py with various for loop examples including break and continue usage. Enhanced whileloop.py with additional logic to print odd numbers and improved output formatting. Modified whileloop11.py to adjust print statement formatting for better output.

--- Thadsha/Python/whileloop.py
+++ Thadsha/Python/whileloop.py
@@ -4,4 +4,11 @@ while x<=9:
         print(x,end='')
         
     x+=1
+
+print("\n********")
+x=1
+while x<=9:
+    if(x%2==1):
+        print(x,end='')
         
+    x+=2  
--- Thadsha/Python/whileloop11.py
+++ Thadsha/Python/whileloop11.py
@@ -2,7 +2,7 @@ x=1
 while x<=5:
     y=1
     while y<=x:
-        print(y,end = ',' if y < x else '')
+        print(y,end = '' if y < x else '')
         y+= 1
     print()
     x+=1
