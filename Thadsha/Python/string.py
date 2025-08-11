34c121f975470ae290b265a766247a20d3fe668f
Thadshajini29 <sajiiselvi@gmail.com>
2025-08-11 16:45:06 +0530

    string function

---
+++ Thadsha/Python/string.py
@@ -0,0 +1,43 @@
+x="Welcome to Python"
+print(x)
+print(len(x))
+print(x[0])
+print(x[-4])
+
+print("\n********")
+
+'''i=0
+while i<len(x):
+    print(x[i])
+    i+=1
+    
+print("\n********")
+
+y=len(x)-1
+while y>=0:
+    print(x[y])
+    y-=1
+
+print("\n********")
+
+t=abs(len(x))
+while (t<=-1):
+    print(x[t])
+    t=t+1'''
+    
+'''for i in x:
+    print(i)
+    
+for i in range(0,len(x),1):
+    print(x[i])
+    
+print("\n********")
+
+for i in reversed(x):
+    print(i)
+
+a=x[8:10]
+print(a)'''
+
+a=x[-17:-10]
+print(a)
