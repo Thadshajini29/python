a16ca10cff866229629b1417253955bf6b29e781
Thadshajini29 <sajiiselvi@gmail.com>
2025-08-14 15:11:43 +0530

    Update validation.py

--- Thadsha/Python/validation.py
+++ Thadsha/Python/validation.py
@@ -1,4 +1,50 @@
-num=input("Please enter number")
+'''num=input("Please enter number")
 while not num.isdigit():
     print('Please enter correct number')
-    num=input('please enter number')
+    num=input('please enter number')'''
+    
+fname=input("Enter Your First Name: ")
+while any(x.isdigit()for x in fname):
+    print("Correct Your First Name")
+    fname=input("Enter Your First Name: ")
+print("first name is:",fname)
+
+lname=input("Enter Your Last Name: ")
+while any(y.isdigit()for y in fname):
+    print("Correct Your Last Name")
+print("Last name is:",lname)
+
+age=input("Enter Your Age:")
+while not age.isdigit():
+    age=input('Enter Your age correct format')
+print("Your Age:",age)
+
+nic=input("Enter Your NIC Number: ")
+if len(nic)==10:
+    year=int("19" + nic[:2])
+    days=int(nic[2:5])
+elif len(nic)==12 and nic.isdigit():
+    year=int(nic[:4])
+    days=int(nic[4:7])
+else:
+    print("Invalid NIC ")
+    exit()
+if days>500:
+    gender="Female"
+    days-=500
+else:
+    gender="Male"
+    
+import datetime
+dob_date=datetime.datetime(year, 1, 1)+datetime.timedelta(days=days - 1)
+dob_str=dob_date.strftime("%Y-%m-%d")
+today=datetime.date.today()
+
+cal_age=today.year - dob_date.year-((today.month, today.day)<(dob_date.month, dob_date.day))
+if cal_age != age:
+    print("Age or NIC wrong")
+    exit()
+   
+print("Your Date of Birth:",dob_date.strftime("%d %B %Y"))
+print("Your Gender:", gender)
+print("Your Age:", cal_age)
